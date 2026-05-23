851d0f4d8ce70d0fe115c2942a1cea90995baa64
Abhiram <abhirampariyapurath@gmail.com>
2026-05-23 12:24:25 +0530

    python sample

---
+++ sum.py
@@ -0,0 +1,5 @@
+a=input("enter 2 numbers")
+b=input()
+
+sum = int(a) + int(b)
+print("the sum is "+str(sum))
---
+++ topic 1.py
@@ -0,0 +1,25 @@
+#identifiers
+
+#comment-->use hashtags
+
+print("hello")
+print("hi abhiram")
+
+a=10
+b=20 #no need to declare type
+
+print(a)
+print(b)
+print("first value is "+str(a)+" second value is "+str(b))
+
+#here we cant combine to print string in double quote and the integer at same time
+#so we make it print as a string
+
+c=input("Enter a number")
+print(c)
+
+d=input("enter 2 numbers")
+e=input()
+
+sum = int(d) + int(e)
+print("the sum is "+str(sum))
