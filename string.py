d2a5febf47ee91758fda04deaff7857e5dc328c9
Abhiram <abhirampariyapurath@gmail.com>
2026-05-23 12:49:52 +0530

    python sample

---
+++ string.py
@@ -0,0 +1,11 @@
+a="hi, how are you"
+
+print(len(a))
+
+print(a[5]) #to print character of a string in 5th position
+
+print(a[4:7]) #to print characters from a location to another only
+
+print(a[-6:-1])
+
+print(a.upper())
