05bb6b9a743a5a4d021f77595ceb0b23bbf6a03c
Abhiram <abhirampariyapurath@gmail.com>
2026-05-23 12:38:45 +0530

    python sample

---
+++ average.py
@@ -0,0 +1,7 @@
+number1: int=int(input("enter 3 numbers"))
+number2=int(input())
+number3=int(input())
+
+average= number1 + number2 + number3 / 3
+
+print("the average is "+str(average))
---
+++ swapping.py
@@ -0,0 +1,13 @@
+a=input("enter the first number")
+b=input("enter the second number")
+
+print(type(a))
+print(type(b))
+#to know what type is the variable
+
+t=a
+a=b
+b=t
+
+print("first number is "+str(a))
+print("second number is "+str(b))
